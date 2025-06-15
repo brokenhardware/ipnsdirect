7f3585cd01da8bd23cdd56da38dfa8aad24805ba
brokenhardware <brokenhardware@proton.me>
2025-06-15 19:55:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { verifiedFetch } from 'https://esm.sh/@helia/verified-fetch?bundle-deps'
+
+let ipnsAddr = "k51qzi5uqu5dhp48cti0590jyvwgxssrii0zdf19pyfsxwoqomqvfg6bg8qj3s";
+const param = window.location.hash.substring(1);
+if (param.length > 0) {ipnsAddr = param;}
+
 const output = document.getElementById('output')
 
-const resp = await verifiedFetch('ipns://k51qzi5uqu5dhp48cti0590jyvwgxssrii0zdf19pyfsxwoqomqvfg6bg8qj3s')
+const resp = await verifiedFetch('ipns://' + ipnsAddr)
 const blob = await resp.blob()
 const imgEl = document.createElement('img')
 imgEl.width = 600;
