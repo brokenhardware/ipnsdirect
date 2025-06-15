ed09dd54cef6dc06a0fe7cea26e52f15a8f26a06
brokenhardware <brokenhardware@proton.me>
2025-06-15 19:45:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ const output = document.getElementById('output')
 const resp = await verifiedFetch('ipns://k51qzi5uqu5dhp48cti0590jyvwgxssrii0zdf19pyfsxwoqomqvfg6bg8qj3s')
 const blob = await resp.blob()
 const imgEl = document.createElement('img')
+imgEl.width = 600;
 imgEl.setAttribute('src', URL.createObjectURL(blob))
 output.appendChild(imgEl)
